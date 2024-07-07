f4caffe1af7a3fe32b49c072d2400954d59498c9
Acidicts <111660544+Acidicts@users.noreply.github.com>
2024-07-07 12:36:21 +0100

    Upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = "Test Email"
+body = "This is a test email"
+sender_email = input("Enter your email: ")
+receiver_email = input("Enter the receiver's email: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.zoho.eu", 465, context=context) as server:
+    server.login(sender_email, input("Enter your password: "))
+    server.send_message(message)
