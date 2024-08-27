3edce642757040a7a9a27325e96e80453d9e0cf7
Acidicts <111660544+Acidicts@users.noreply.github.com>
2024-08-27 18:21:25 +0100

    Fix

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ subject = input("Enter the subject of the email: ")
 body = input("Enter the body of the email: ")
 sender_email = input("Enter your email: ")
 receiver_email = input("Enter the receiver's email: ")
+host = input("Enter the host: ")
 
 message = EmailMessage()
 message["From"] = sender_email
@@ -15,6 +16,6 @@ message.set_content(body)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL("smtp.zoho.eu", 465, context=context) as server:
+with smtplib.SMTP_SSL(host, 465, context=context) as server:
     server.login(sender_email, input("Enter your password: "))
     server.send_message(message)
